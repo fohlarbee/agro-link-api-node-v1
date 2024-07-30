fc5dc78ae8f663dd2ac4fb6f44289a2a5fe4aaeb
sammyola246 <sammyola246@gmail.com>
2024-07-30 22:13:25 +0100

    webhook

--- src/transactions/interceptors/headers.interceptor.ts
+++ src/transactions/interceptors/headers.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
-import crypto from "crypto";
+import { createHmac } from "crypto";
 
 @Injectable()
 export class PaystackAuthInterceptor implements NestInterceptor {
@@ -18,11 +18,11 @@ export class PaystackAuthInterceptor implements NestInterceptor {
   ): Promise<Observable<any>> {
     const request = context.switchToHttp().getRequest();
     const transactionPayload = JSON.stringify(request.body);
-    console.log(crypto);
-    const hash = crypto
-      .createHmac("sha512", process.env.PSK_SECRET_KEY)
+    // console.log(crypto);
+    const hash = createHmac("sha512", process.env.PSK_SECRET_KEY)
       .update(transactionPayload)
       .digest("hex");
+      console.log(hash);
     if (hash != request.headers["x-paystack-signature"])
       throw new UnauthorizedException();
     return next.handle().pipe(
